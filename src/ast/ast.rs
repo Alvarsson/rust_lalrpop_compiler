c709ae4be7dce91688f3bf462221ccc6769107eb
Alvarsson <a-alvarsson@hotmail.com>
2020-09-19 16:05:30 +0200

    Remakedone, one little error still remaining. Somehow i cant make functions as fn a(..), only as fn a (..). Otherwise it takes

--- src/ast/ast.rs
+++ src/ast/ast.rs
@@ -1,6 +1,194 @@
 // ast
 use std::fmt;
 
+#[derive(Debug)]
+pub enum Exprs {
+    Boolean(bool),
+    Number(i32),
+    Id(String),
+    NotOp(Op, Box<Exprs>),
+    Op(Box<Exprs>, Op, Box<Exprs>),
+    FunctionCall(String, Vec<Box<Exprs>>), // Uuuhh FUNKAR INTE!! Ambiguity prob...
+}
+
+impl fmt::Display for Exprs {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Exprs::Number(i) => write!(f, "{}", i)?,
+            Exprs::Id(s) => write!(f, "{}", s)?,
+            Exprs::Boolean(b) => write!(f, "{}", b)?,
+            Exprs::NotOp(not,exp) => write!(f, "{}{}",not,exp)?,
+            Exprs::FunctionCall(s,vec) => { 
+                write!(f, "{}", s)?;
+                for (i,v) in vec.iter().enumerate() {
+                    write!(f,"{}", v)?;
+                    if i < vec.len()-1 {
+                        write!(f, "")?;
+                    }
+                }
+            }
+            Exprs::Op(expr,op,expr2) => write!(f, "({} {} {})", expr, op, expr2)?,
+        };
+        Ok(())
+    }
+} 
+
+#[derive(Debug)]
+pub enum Op {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    And,
+    Or,
+    Eq,
+    Neq,
+    Gtr,
+    Lss,
+    Geq,
+    Leq,
+    Not,
+}
+impl fmt::Display for Op {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Op::Add => write!(f, "{}", "+")?,
+            Op::Sub => write!(f, "{}", "-")?,
+            Op::Mul => write!(f, "{}", "*")?,
+            Op::Div => write!(f, "{}", "/")?,
+            Op::And => write!(f, "{}", "&&")?,
+            Op::Or => write!(f, "{}", "||")?,
+            Op::Eq => write!(f, "{}", "==")?,
+            Op::Neq => write!(f, "{}", "!=")?,
+            Op::Gtr => write!(f, "{}", ">")?,
+            Op::Lss => write!(f, "{}", "<")?,
+            Op::Geq => write!(f, "{}", ">=")?,
+            Op::Leq => write!(f, "{}", "<=")?,
+            Op::Not => write!(f, "{}", "!")?,
+        };
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+pub enum Type {
+    I32,
+    Bool,
+    Unit,
+}
+impl fmt::Display for Type {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Type::Bool => write!(f, "{}", "bool")?,
+            Type::I32 => write!(f, "{}", "i32")?,
+            Type::Unit => write!(f, "{}", "()")?,
+        };
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+pub enum Statement {
+    Let(bool, String, Option<Type>, Option<Box<Exprs>>),
+    Cond(AllCond, Option<Box<Exprs>>, Box<Statement>, Option<Box<Statement>>),
+    Block(Vec<Box<Statement>>),
+    While(Box<Exprs>, Box<Statement>),
+    Assign(String, Box<Exprs>),
+    Return(Box<Exprs>),
+    Exprs(Box<Exprs>),
+    Function(String, Vec<Box<Statement>>, Option<Type>, Box<Statement>),
+    FuncArg(String, Type),
+}
+impl fmt::Display for Statement { //Statement with optional
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Statement::Let(m, id,optype,opexp) => {
+                write!(f, "let ")?;
+                if *m {
+                    write!(f, "mut ")?;
+                }
+                write!(f, "{}", id)?;
+                if let Some(typ) = optype {
+                    write!(f, ": {} ", typ)?;
+                }
+                if let Some(exp) = opexp {
+                    write!(f, "= {};", exp)?;
+                }
+            }
+            Statement::Return(expr) => {
+                write!(f, "{}", expr)?;
+            }
+            Statement::Block(state)  => {
+                write!(f, "{{\n")?;
+                for (i,stmt) in state.iter().enumerate(){
+                    write!(f, "        {}\n", stmt)?;
+                    if i <state.len()-1 {
+                        write!(f, ";\n")?;
+                    }
+                }
+                write!(f, "    }}")?;
+            } 
+            Statement::Cond(cond,opexpr,stmt,opstmt) => {
+                write!(f, "{} ", cond)?;
+                if let Some(e) = opexpr {
+                    write!(f, "{} ", e)?;
+                }
+                write!(f, "{}", stmt)?;
+                if let Some(s) = opstmt {
+                    write!(f, "{}", s)?;
+                }
+            }
+            Statement::While(expr,block) => {
+                write!(f, "while {} {}", expr, block)?;
+            }
+            Statement::Assign(id,expr) => {
+                write!(f, "{} = {};", id, expr)?;
+            }
+            Statement::Function(id, arg,optyp,block) => {
+                write!(f, "fn {}(", id)?;
+                for (i,a) in arg.iter().enumerate(){
+                    write!(f, "{}", a)?;
+                    if i < arg.len()-1 {
+                        write!(f, ", ")?;
+                    } 
+                }
+                write!(f, ") ")?;
+                if let Some(t) = optyp {
+                    write!(f, "-> {} ", t)?;
+                }
+                write!(f, "{}", block)?;
+            }
+            Statement::Exprs(ex) => {
+                write!(f, "{}", ex)?;
+            }
+            Statement::FuncArg(id,typ) => {
+                write!(f, "{}:{}", id, typ)?;
+            }
+        };
+        Ok(())
+    }
+} 
+
+#[derive(Debug)]
+pub enum AllCond {
+    If,
+    Else,
+    ElseIf,
+}
+impl fmt::Display for AllCond {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            AllCond::If => write!(f, "{}", "if")?,
+            AllCond::ElseIf => write!(f, "{}", "else if")?,
+            AllCond::Else => write!(f, "{}", "else")?
+        };
+        Ok(())
+    }
+}
+
+
+
+/*
 #[derive(Debug)] // debug-attribut, ungefär som arv i Java
 pub enum NumOrId {
     Num(i32),
@@ -251,7 +439,7 @@ impl fmt::Display for Expr {
     }
 } 
 
-
+*/
 
 
 
--- src/ast/main.rs
+++ src/ast/main.rs
@@ -8,68 +8,53 @@ pub mod ast;
 
 fn main() {
     println!("minimal");
-    let tester = "fn _if_then_else_and_while() {
-        // a function taking two bool arguments returning the bool type
-        // with some let statements and function calls
-        fn a(x: bool, y: bool) -> bool {
-            if x && y {
-                let a: bool = true;
-                y || a
-            } else {
-                x && false
-            }
-        } 
-        //CHECK
-    
-        // a function taking two bool arguments returning the i32 type
-        // with some let statements and function calls
-        fn b(x: bool, y: bool) -> i32 {
-            let a: bool = a(x, y || false);
-            let mut b: i32 = 0;
-            if a && y {
-                let a: bool = true; // shadowing
-                if y || a {
-                    b = b + 1;
-                };
-            } else {
-                if !(x && false) {
-                    b = b - 1;
-                }
-            };
-            b + 3
-        }
-        //CHECK
-    
-        // a function taking two bool arguments returning the i32 type
-        // while
-        fn c(x: bool, y: bool) -> i32 {
-            let mut b: i32 = 0;
-            let mut c: i32 = 1;
-            while (b < 10) {
-                c = c * 2;
-            }
-            c
-        }
-    }";
-    //println!("{:?}", ProgramParser::new().parse(tester));
 }
 
 #[test]
 fn parse_let() {
-    println!("{:?}", StmtParser::new().parse("
-    !(x && false)"));
-    println!("{:?}", StmtParser::new().parse("
-    fn c(x: bool, y: bool) -> i32 {
+    println!("{:?}", StmtsParser::new().parse("
+    // a function taking two bool arguments returning the bool type
+    // with some let statements and function calls
+    fn a (x: bool, y: bool) -> bool {
+        if x && y {
+            let a: bool = true;
+            y || a
+        } else {
+            x && false
+        }
+    } 
+    "));
+    println!("{:?}", StmtsParser::new().parse("
+    fn b (x: bool, y: bool) -> i32 {
+        let a: bool = a(x, y || false);
+        let mut b: i32 = 0;
+        if a && y {
+            let a: bool = true; // shadowing
+            if y || a {
+                b = b + 1;
+            }
+        } else {
+            if !(x && false) {
+                b = b - 1;
+            }
+        }
+        b + 3
+    }
+    "));
+    println!("{:?}", StmtsParser::new().parse("
+    // a function taking two bool arguments returning the i32 type
+    // while
+    fn c (x: bool, y: bool) -> i32 {
         let mut b: i32 = 0;
         let mut c: i32 = 1;
         while (b < 10) {
             c = c * 2;
-        };
+        }
         c
-    }" 
-));
+    }
+    "));
 }
-#[test]
+
 fn _let_and_return() {
     // a function taking no arguments returning the unit type
     fn a() -> () {
