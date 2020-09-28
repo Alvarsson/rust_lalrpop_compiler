2074c39bba5ce2a25d6625aaf2fae2a021db9713
Albin Thomsson <albtho-5@student.ltu.se>
2020-09-28 14:51:00 +0200

    code cleanup

--- src/ast/type_check.rs
+++ src/ast/type_check.rs
@@ -10,10 +10,10 @@ type Error = String;
 
 fn expr_check(expr: Box<Exprs>, scope: &mut Scope) -> Result<Type, Error> {
     match *expr {
-        ast::Exprs::Boolean(_) => Ok(ast::Type::Bool),
-        ast::Exprs::Number(_) => Ok(ast::Type::I32),
-        ast::Exprs::Id(id) => scope.get_symbol(&id), // Go through depending on type. This means early reterun though.
-        ast::Exprs::Op(e1,o,e2) => {
+        Exprs::Boolean(_) => Ok(Type::Bool),
+        Exprs::Number(_) => Ok(Type::I32),
+        Exprs::Id(id) => scope.get_symbol(&id), // Go through depending on type. This means early reterun though.
+        Exprs::Op(e1,o,e2) => {
             let recurExpr1 = expr_check(e1, scope);
             let recurExpr2 = expr_check(e2, scope);
             if recurExpr1.is_err() {
@@ -25,62 +25,62 @@ fn expr_check(expr: Box<Exprs>, scope: &mut Scope) -> Result<Type, Error> {
             let type1 = recurExpr1.unwrap(); // tar ut typerna om inget err.
             let type2 = recurExpr2.unwrap();
             match o { 
-                ast::Op::Gtr | ast::Op::Lss | ast::Op::Geq | ast::Op::Leq => {
-                    if type1 == ast::Type::I32 && type2 == ast::Type::I32 {
-                        return Ok(ast::Type::Bool)
+                Op::Gtr | Op::Lss | Op::Geq | Op::Leq => {
+                    if type1 == Type::I32 && type2 == Type::I32 {
+                        return Ok(Type::Bool)
                     } 
                     return Err(format!("Could not do {:?} for types {:?} and {:?}", o, e1,e2))
                 },
-                ast::Op::Add | ast::Op::Sub | ast::Op::Mul | ast::Op::Div => {
-                    if type1 == ast::Type::I32 && type2 == ast::Type::I32 {
-                        return Ok(ast::Type::Bool)
+                Op::Add | Op::Sub | Op::Mul | Op::Div => {
+                    if type1 == Type::I32 && type2 == Type::I32 {
+                        return Ok(Type::Bool)
                     }
                     return Err(format!("Cound not do {:?} for types {:?} and {:?}", o, e1,e2))
                 },
-                ast::Op::And | ast::Op::Or => {
-                    if type1 == ast::Type::Bool && type2 == ast::Type::Bool {
-                        return Ok(ast::Type::Bool)
+                Op::And | Op::Or => {
+                    if type1 == Type::Bool && type2 == Type::Bool {
+                        return Ok(Type::Bool)
                     }
                     return Err(format!("Could not do {:?} for types {:?} and {:?}", o, e1,e2))
                 },
-                ast::Op::Eq => {
+                Op::Eq => {
                     if type1 == type2 {
-                        Ok(ast::Type::Bool)
+                        Ok(Type::Bool)
                     }
                     return Err(format!("Could not do {:?} for types {:?} and {:?}", o, e1,e2))
                 },
-                ast::Op::Neq => {
+                Op::Neq => {
                     if type1 != type2 {
-                        Ok(ast::Type::Bool)
+                        Ok(Type::Bool)
                     }
                     return Err(format!("Could not do {:?} for types {:?} and {:?}", o, e1,e2))
                 },
                 _ => Err("NONE OF OP'S".to_string())
             }
         },
-        ast::Exprs::NotOp(ast::Op::Not ,e) => {
+        Exprs::NotOp(Op::Not ,e) => {
             let recEx1 = expr_check(e, scope);
             if recEx1.is_err() {
                 return recEx1;
             }
             let typ = recEx1.unwrap();
-            if typ == ast::Type::Bool {
-                return Ok(ast::Type::Bool)
+            if typ == Type::Bool {
+                return Ok(Type::Bool)
             }
             return Err(format!("Expression {:?} do not support !", typ));
         },
-        ast::Exprs::NotOp(ast::Op::Sub, e) => {
+        Exprs::NotOp(Op::Sub, e) => {
             let recEx1 = expr_check(e, scope);
             if recEx1.is_err() {
                 return recEx1;
             }
             let typ = recEx1.unwrap();
-            if typ == ast::Type::I32 {
-                return Ok(ast::Type::I32)
+            if typ == Type::I32 {
+                return Ok(Type::I32)
             }
             return Err(format!("The type {:?} does not support negative inverting", typ))
         },
-        ast::Exprs::FunctionCall(id, expressions) => {
+        Exprs::FunctionCall(id, expressions) => {
             let mut arguments = vec![]; //instansiate an empty vector for arguments.
             for expr in expressions { // expr_check for each expr.
                 let recur = expr_check(expr, scope);
@@ -101,9 +101,9 @@ fn expr_check(expr: Box<Exprs>, scope: &mut Scope) -> Result<Type, Error> {
 }
 
 //For checking the if, else if, and else statement conditionals.
-pub fn if_else_check(stmt: Box<ast::Statement>, scope: &mut Scope) -> Result<ast::Type, Error> {
+pub fn if_else_check(stmt: Box<Statement>, scope: &mut Scope) -> Result<Type, Error> {
     match *stmt {
-        ast::Statement::Cond(ast::AllCond::If, Some(e), block, None) => {
+        Statement::Cond(AllCond::If, Some(e), block, None) => {
             let rec = expr_check(e, scope);
             if rec.is_err() {
                 Err(format!("Incorrect if statement, {:?}", rec))
@@ -112,7 +112,7 @@ pub fn if_else_check(stmt: Box<ast::Statement>, scope: &mut Scope) -> Result<ast
                 //TODO: BLOCK TYPE CHECK
             }
         },
-        ast::Statement::Cond(ast::AllCond::ElseIf, Some(e), block, Some(st)) => {
+        Statement::Cond(AllCond::ElseIf, Some(e), block, Some(st)) => {
             let rec = expr_check(e, scope);
             if rec.is_err() {
                 Err(format!("Incorrect else if statement, {:?}", rec))
@@ -125,17 +125,17 @@ pub fn if_else_check(stmt: Box<ast::Statement>, scope: &mut Scope) -> Result<ast
     }
     
 }
-pub fn block_check(block: Box<ast::Statement>, scope: &mut Scope) -> Result<ast::Type, Error> {
+pub fn block_check(block: Box<Statement>, scope: &mut Scope) -> Result<Type, Error> {
     //Will need scope...need to do that before block.
 
     //First we enter block, ie go into new scope
     scope.addLayer();
 
     let opReturn = match *block {
-        ast::Statement::Block(stmt, Some(ret)) => { //with  return
+        Statement::Block(stmt, Some(ret)) => { //with  return
             //check that only statements are in the scope with the type of expl/impl return
         },
-        ast::Statement::Block(stmt, None) => { //No implicit/explicit return
+        Statement::Block(stmt, None) => { //No implicit/explicit return
             //need only recurse to statement check.
         }
     };
@@ -153,17 +153,17 @@ pub fn block_check(block: Box<ast::Statement>, scope: &mut Scope) -> Result<ast:
     
 }
 
-pub fn function_check() -> Result<ast::Type, Error> {
+pub fn function_check() -> Result<Type, Error> {
 
 
     //The return statement should check against the block check...
 
 }
 
-pub fn statement_check(stmts: Vec<Box<Statement>>, scope: &mut Scope) -> Result<ast::Type, Error> {
+pub fn statement_check(stmts: Vec<Box<Statement>>, scope: &mut Scope) -> Result<Type, Error> {
     for stmt in stmts {
         match *stmt {
-            ast::Statement::Assign(id, ex) => {
+            Statement::Assign(id, ex) => {
                 let sAssign = scope.get_symbol(&id);
                 if sAssign.is_err() {
                     sAssign
@@ -173,10 +173,10 @@ pub fn statement_check(stmts: Vec<Box<Statement>>, scope: &mut Scope) -> Result<
                     if s2Assign.is_err() {
                         s2Assign
                     }
-                    Ok(ast::Type::Unit)
+                    Ok(Type::Unit)
                 }
             },
-            ast::Statement::While(ex, block) => {
+            Statement::While(ex, block) => {
                 //First check expression 
                 let testEx = expr_check(ex);
                 if testEx.is_err() {
@@ -184,15 +184,15 @@ pub fn statement_check(stmts: Vec<Box<Statement>>, scope: &mut Scope) -> Result<
                 }
                 else {
                     let typ = testEx.unwrap();
-                    if typ == ast::Type::Bool {
+                    if typ == Type::Bool {
                         let testBlock = block_check(block, scope);
                         if testBlock.is_err() {
                             testBlock
                         }
                         else {
                             let typ2 = testBlock.unwrap();//should be unit for block unless return
-                            if typ2 == ast::Type::Unit {
-                                Ok(ast::Type::Unit)
+                            if typ2 == Type::Unit {
+                                Ok(Type::Unit)
                             }
                             else {
                                 Err(format!("Not a Unit return, instead got: {}", typ2))
@@ -204,7 +204,7 @@ pub fn statement_check(stmts: Vec<Box<Statement>>, scope: &mut Scope) -> Result<
                     }
                 }
             },
-            ast::Statement::Let(m,id,)
+            Statement::Let(m,id,)
 
         }
     }
@@ -217,13 +217,13 @@ pub fn statement_check(stmts: Vec<Box<Statement>>, scope: &mut Scope) -> Result<
 pub struct Scope {
     scope_layer: i32, // Scope Layer identification
     table: HashMap<i32, HashMap<String, SignatureType>>, //Two maps with the signature type in one.
-    symbolTable: HashMap<i32, HashMap<String, ast::Type>>,
+    symbolTable: HashMap<i32, HashMap<String, Type>>,
     src: String,
 }
 
 struct SignatureType { // either an argument or return
-    arg: Vec<ast::Type>,
-    retur: ast::Type,
+    arg: Vec<Type>,
+    retur: Type,
 }
 
 impl Scope {
@@ -245,11 +245,11 @@ impl Scope {
         self.table.remove(&self.scope_layer);
         self.scope_layer -= 1;
     }
-    fn register(&mut self, id: &String, args: Vec<::ast::Type>, ret: ast::Type) {
+    fn register(&mut self, id: &String, args: Vec<::Type>, ret: Type) {
         let scope_layer = self.table.get_mut(&self.scope_layer).unwrap();
         scope_layer.insert(id.to_string(), SignatureType{arg: arg, retur: retur});
     }
-    fn register_symbol(&mut self, id: &String, ret: ast::Type) { // Add symbol, this will make shadowing/borrow possible aswell
+    fn register_symbol(&mut self, id: &String, ret: Type) { // Add symbol, this will make shadowing/borrow possible aswell
         let scope_layer = self.symbolTable.get_mut(&self.scope_layer).unwrap();
         scope_layer.insert(id.to_string(), SignatureType{arg: arg, retur: retur});
     }
@@ -258,7 +258,7 @@ impl Scope {
 
 
 
-    fn get_symbol(&mut self, id: &String) -> Result<ast::Type, Error> { //Check variable in scope.
+    fn get_symbol(&mut self, id: &String) -> Result<Type, Error> { //Check variable in scope.
         let mut currentSymbol = self.scope_layer;
         while currentSymbol >= 0 {
             let scope_layer = self.symbolTable.get(&currentSymbol).unwrap();
@@ -275,7 +275,7 @@ impl Scope {
 
 
 
-    fn get_func(&mut self, id: &String, args: Vec<ast::Type>) -> Result<ast::Type, Error> {
+    fn get_func(&mut self, id: &String, args: Vec<Type>) -> Result<Type, Error> {
         let mut currentfunc = self.scope_layer;
         while currentfunc >= 0 {
             let scope_layer = self.table.get(&currentfunc).unwrap();
